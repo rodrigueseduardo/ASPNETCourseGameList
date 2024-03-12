1fb40ca88e7dfe752e1132e0b55f7aa23e950197
rodrigueseduardo <eduxdudu@icloud.com>
2024-03-12 14:33:41 -0300

    Adicao do método de tipos de jogos

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -9,4 +9,16 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
     public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genres => Set<Genre>();
-}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new {Id = 1, Name = "Fighting" },
+            new {Id = 2, Name = "RPG" },
+            new {Id = 3, Name = "Adventure" },
+            new {Id = 4, Name = "Platform" },
+            new {Id = 5, Name = "Sports" },
+            new {Id = 6, Name = "Racing" }
+        );
+    }
+};
