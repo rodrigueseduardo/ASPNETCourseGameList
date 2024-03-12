932fa933406333614401a74c1d85da9455571a22
rodrigueseduardo <eduxdudu@icloud.com>
2024-03-11 21:46:14 -0300

    Implementado a Database

--- obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e9b4656c70925abf972598eaf39ac9d9eb1308")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
